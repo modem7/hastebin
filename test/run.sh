72a6a1e2efa1e97e6be86a5121a877f0326d4e3b
modem7 <modem7@gmail.com>
2021-09-01 17:29:21 +0100

    Update run.sh

--- test/run.sh
+++ test/run.sh
@@ -5,7 +5,7 @@ cat > /app/config.js <<EOF
 {
   "host": "${HOST:-0.0.0.0}",
   "port": ${PORT:-7777},
-  "keyLength": ${KEY_LENGTH:-5},
+  "keyLength": ${KEY_LENGTH:-10},
   "maxLength": ${MAX_LENGTH:-400000},
   "staticMaxAge": ${STATIC_MAX_AGE:-86400},
   "recompressStaticAssets": ${RECOMPRESS_STATIC_ASSETS:-true},
@@ -13,20 +13,12 @@ cat > /app/config.js <<EOF
     {
       "level": "${LOGGING_LEVEL:-verbose}",
       "type": "${LOGGING_TYPE:-Console}",
-      "colorize": ${LOGGING_COLORIZE:-true}
+      "colorize": ${LOGGING_COLORIZE:-false}
     }
   ],
   "keyGenerator": {
     "type": "${KEY_GENERATOR_TYPE:-phonetic}"
   },
-  "rateLimits": {
-    "categories": {
-      "normal": {
-        "totalRequests": "$RATE_LIMIT_TOTAL:-500}",
-        "every": "$RATE_LIMIT_EVERY:-60000}"
-            }
-        }
-    },
   "storage": {
     "type": "${STORAGE_TYPE:-file}",
     "path": "${STORAGE_PATH:-./data}",
