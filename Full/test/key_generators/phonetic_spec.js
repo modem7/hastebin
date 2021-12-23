b43b92b62364222d7b6ebba8e9cdb502a4545a48
modem7 <modem7@gmail.com>
2021-12-23 02:42:16 +0000

    updated from https://github.com/JoCat/haste-server

---
+++ Full/test/key_generators/phonetic_spec.js
@@ -0,0 +1,34 @@
+/* global describe, it */
+
+const assert = require('assert');
+
+const Generator = require('../../lib/key_generators/phonetic');
+
+const vowels = 'aeiou';
+const consonants = 'bcdfghjklmnpqrstvwxyz';
+
+describe('RandomKeyGenerator', () => {
+  describe('randomKey', () => {
+    it('should return a key of the proper length', () => {
+      const gen = new Generator();
+      assert.equal(6, gen.createKey(6).length);
+    });
+
+    it('should alternate consonants and vowels', () => {
+      const gen = new Generator();
+
+      const key = gen.createKey(3);
+      const consonantStart = consonants.includes(key[0])
+
+      if (consonantStart) {
+        assert.ok(consonants.includes(key[0]));
+        assert.ok(consonants.includes(key[2]));
+        assert.ok(vowels.includes(key[1]));
+      } else {
+        assert.ok(vowels.includes(key[0]));
+        assert.ok(vowels.includes(key[2]));
+        assert.ok(consonants.includes(key[1]));
+      }
+    });
+  });
+});
